61104bc00ef3920de731d5ccd350cf91245de7a8
James Churchill <james@smashdev.com>
2013-01-09 00:26:52 -0800

    Removed files and code

--- Zappos/Zappos/Common/PitchItemDataTemplateSelector.cs
+++ Zappos/Zappos/Common/PitchItemDataTemplateSelector.cs
@@ -36,12 +36,7 @@ namespace Zappos.Common
 						throw new Exception("Unexpected PitchItemLayout enum value: " + pitchItem.Layout.ToString());
 				}
 			}
-
-			//SetColumnRowSpan(uiElement, 1, 1);
 			return ItemTemplateStandard;
-
-			// JCTODO remove???
-			//return null;
 		}
 
 		private void SetColumnRowSpan(UIElement uiElement, int columnSpan, int rowSpan)
@@ -50,43 +45,4 @@ namespace Zappos.Common
 			VariableSizedWrapGrid.SetRowSpan(uiElement, rowSpan);
 		}
 	}
-
-	// JCTODO remove
-	//public class PitchItemTemplateSelector : TemplateSelector
-	//{
-	//	public DataTemplate ItemTemplateStandard { get; set; }
-	//	public DataTemplate ItemTemplateTall { get; set; }
-	//	public DataTemplate ItemTemplateWide { get; set; }
-
-	//	public override DataTemplate SelectTemplate(object item, DependencyObject container)
-	//	{
-	//		var pitchItem = item as PitchItem;
-	//		var uiElement = container as UIElement;
-	//		if (pitchItem != null && uiElement != null)
-	//		{
-	//			switch (pitchItem.Layout)
-	//			{
-	//				case PitchItemLayout.Standard:
-	//					SetColumnRowSpan(uiElement, 1, 1);
-	//					return ItemTemplateStandard;
-	//				case PitchItemLayout.Tall:
-	//					SetColumnRowSpan(uiElement, 1, 2);
-	//					return ItemTemplateTall;
-	//				case PitchItemLayout.Wide:
-	//					SetColumnRowSpan(uiElement, 2, 1);
-	//					return ItemTemplateWide;
-	//				default:
-	//					throw new Exception("Unexpected PitchItemLayout enum value: " + pitchItem.Layout.ToString());
-	//			}
-	//		}
-
-	//		return null;
-	//	}
-
-	//	private void SetColumnRowSpan(UIElement uiElement, int columnSpan, int rowSpan)
-	//	{
-	//		VariableSizedWrapGrid.SetColumnSpan(uiElement, columnSpan);
-	//		VariableSizedWrapGrid.SetRowSpan(uiElement, rowSpan);
-	//	}
-	//}
 }
